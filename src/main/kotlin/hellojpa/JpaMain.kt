668d8b952ea7351559b83ce1b1fd9a0e533c59dc
tykan <kang3603a@gmail.com>
2022-08-17 16:29:45 +0900

    .

---
+++ src/main/kotlin/hellojpa/JpaMain.kt
@@ -0,0 +1,41 @@
+package hellojpa
+
+import javax.persistence.EntityManager
+import javax.persistence.EntityManagerFactory
+import javax.persistence.EntityTransaction
+import javax.persistence.Persistence
+
+class JpaMain{
+    companion object{
+        @JvmStatic fun main(args: Array<String>){
+            val emf: EntityManagerFactory = Persistence.createEntityManagerFactory("hello")
+
+            val em : EntityManager = emf.createEntityManager()
+
+            val tx : EntityTransaction = em.transaction
+            tx.begin()
+            try {
+//                val member = Member(id = 2L, name= "HelloB")
+//                em.persist(member)
+//               val member = em.find(Member::class.java, 2L)
+//                member.name = "HelloJPA"
+//
+//                em.remove(member)
+                val findMember = em.createQuery("select  m from Member m", Member::class.java).run {
+                    firstResult = 5
+                    maxResults = 100
+                    resultList
+                }
+
+                tx.commit()
+            }catch (e: Exception){
+                tx.rollback()
+            } finally {
+                em.close()
+            }
+            emf.close()
+        }
+    }
+}
+
+
---
+++ src/main/kotlin/jpabasic/ex1hellojpa/Ex1HelloJpaApplication.kt
@@ -0,0 +1,11 @@
+package jpabasic.ex1hellojpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+//@SpringBootApplication
+//class Ex1HelloJpaApplication
+//
+//fun main(args: Array<String>) {
+//    runApplication<Ex1HelloJpaApplication>(*args)
+//}
